92148147d467e79a73bf216494ef34d53eaba7ca
Etienne223 <etiennelfvre@gmail.com>
2021-02-17 09:33:40 +0100

    Test

--- js/cookie.js
+++ js/cookie.js
@@ -4,31 +4,31 @@ var points = 0;
 //variable d'amélioration du clique
 var a = 1;
 //variable de productions
-var prodChocChiffre = 100;
+var prodChocChiffre = 0;
 //variable de niveaux
 var levelChocChiffre = 0;
 //variable de couts
-var prixChocChiffre = 100;
+var prixChocChiffre = 10;
 
 //quand on clique ça monte le score
 document.getElementById("bigClick").addEventListener('click', function plusUn(){
     points = points + a; 
-    CookieCount.innerHTML = points;
+    CookieCount.innerHTML = "You have " + points + " bisouuuus";
     acheterChoc()
 })
 
 //fonction pour les amélioration d'auto click
 document.getElementById("choc").addEventListener('click', function autoClick() {
     setInterval(function(){ points = points + 1; }, 1000);        //<= augmente les points de 1 par seconde
-    setInterval(function(){ CookieCount.innerHTML = points; }, 1000);  //<= actualise les points gagner par seconde
-    prodChocChiffre = prodChocChiffre + 100;
-    prodChoc.innerHTML = "Production actuelle : " + prodChocChiffre + ".000/sec";
+    setInterval(function(){ CookieCount.innerHTML = "You have " + points + " bisouuuus"; }, 1000);  //<= actualise les points gagner par seconde
+    prodChocChiffre = prodChocChiffre + 1;
+    prodChoc.innerHTML = "Production actuelle : " + prodChocChiffre + " bisous/sec";
     levelChocChiffre = levelChocChiffre + 1;
     levelChoc.innerHTML = levelChocChiffre;
     points = points - prixChocChiffre;
     prixChocChiffre = prixChocChiffre * 2;
     prixChoc.innerHTML = prixChocChiffre + "bisous";
-   
+    acheterChoc();
     })
 
     
